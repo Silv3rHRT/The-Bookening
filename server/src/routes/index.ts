9bcd75bc73ad20d2d794e3551b64f4d13350c8cc
Silv3rHRT <somethingcuteandgay@gmail.com>
2025-02-22 09:29:16 -0500

    uploading starter code

---
+++ server/src/routes/index.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from 'express';
+import express from 'express';
+const router = express.Router();
+
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import apiRoutes from './api/index.js';
+
+router.use('/api', apiRoutes);
+
+//TODO: look at 2/19 class video for fixes to this
+// serve up react front-end in production
+router.use((_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
+
+export default router;
